bd483928236cb1e688a43245b164b2f7a0c553ec
AlpJQ <2951258070@qq.com>
2020-03-01 19:43:40 +0800

    add check cookies

--- src/main/java/life/jiasheng/community/controller/IndexController.java
+++ src/main/java/life/jiasheng/community/controller/IndexController.java
@@ -1,15 +1,38 @@
 package life.jiasheng.community.controller;
 
+import life.jiasheng.community.mapper.UserMappper;
+import life.jiasheng.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class IndexController {
 
+    @Autowired(required = false)
+    private UserMappper userMappper;
+
+    //功能：当用户访问网站的时候，先检查是否登陆过，即验证token是否存在服务器的数据库中
     @GetMapping("/")
-    public String hello(){
+    public String index(HttpServletRequest request){
+        //request会得到很多的cookie，其中只有key为token的数据才是我们想要的
+        //因为我们只有通过token来验证用户的登录过该网站信息
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if("token".equals(cookie.getName())){
+                String token = cookie.getValue();
+                User user = userMappper.finByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 
--- src/main/java/life/jiasheng/community/mapper/UserMappper.java
+++ src/main/java/life/jiasheng/community/mapper/UserMappper.java
@@ -3,6 +3,8 @@ package life.jiasheng.community.mapper;
 import life.jiasheng.community.model.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,7 @@ public interface UserMappper {
 
     @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     public void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User finByToken(@Param("token") String token);
 }
